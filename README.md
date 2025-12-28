4a726a7f160a4e8a25ec24d04e606efba8b6114f
yuekcc <yuekcc@qq.com>
2025-12-29 00:04:57 +0800

    增加更新脚本

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 1. 默认英文输入
 2. 启用输入联想
 
+## 集成 RIME-LMDG 权重
+
+可以从 [RIME-LMDG](https://github.com/amzxyz/RIME-LMDG) 项目下载最新的词库，通过 script 下的脚本更新五笔的词库权重。
+
+> LMDG 项目是基于大规模语料进行标记权重，词库顺序效果理论上更好。
---
+++ scripts/resort.mjs
@@ -0,0 +1,84 @@
+import { Database } from 'bun:sqlite'
+import readline from 'node:readline'
+import fs from 'node:fs'
+
+const db = new Database('rime-lmdg-weights.db')
+
+
+process.on('exit', () => {
+    db.close()
+})
+
+class Word {
+    constructor() {
+        this.id = 0;
+        this.content = '';
+        this.length = 0;
+        this.weight = 0;
+        this.pinyin = '';
+        this.source = '';
+    }
+}
+
+function lookupWeight(keyword) {
+    try {
+        const query = db.query(`select id, content, length, weight, pinyin, source from word where content = $content`).as(Word);
+        return query.get({ $content: keyword })
+    } catch (err) {
+        console.warn('query keyword error, for', err.message, ', keyword is', keyword)
+        return null;
+    }
+}
+
+
+function parseLineByLine(dictFile) {
+    const { promise, resolve } = Promise.withResolvers()
+
+    const cache = [];
+    const stream = fs.createReadStream(dictFile)
+    const rl = readline.createInterface({ input: stream })
+    let enable = false;
+    rl.on('line', line => {
+        if (line === '...') {
+            enable = true;
+            return;
+        }
+
+        if (!enable) {
+            console.log('ignore line:', line)
+            return;
+        }
+
+        if (!line || line.startsWith('#')) {
+            return;
+        }
+        const cols = line.split('\t')
+        if (cols.length < 3) {
+            console.warn(`Expected length >= 3, got ${cols.length}, line = ${line}`)
+            return
+        }
+
+        const [text, code, weight, stem] = cols;
+        const newWeight = lookupWeight(text)?.weight ?? weight;
+
+        if (stem) {
+            cache.push([text, code, newWeight, stem].join('\t') + '\n')
+        } else {
+            cache.push([text, code, newWeight].join('\t') + '\n')
+        }
+    })
+
+    rl.on('close', () => {
+        resolve(cache)
+    })
+
+    return promise;
+}
+
+const lines = await parseLineByLine('wubi.dict.yaml');
+const stream = fs.createWriteStream('out.yaml')
+for (const line of lines) {
+    stream.write(line, 'utf-8')
+}
+stream.close()
+
